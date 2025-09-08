ada8698a6611daf4242ab8955012794522438456
lunawhisperswithyou-png <lunawhisperswithyou@gmail.com>
2025-09-08 06:44:17 +0500

    Black dragon solana token creator

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Black-Dragon
-Solana Launchpad
+# Black Dragon Solana Token Creator
+
+This project allows you to create Solana tokens easily. Follow the instructions to set up and use the application.
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Black Dragon Token Creator</title>
+        <meta name="description" content="Create your Solana tokens easily." />
+      </Head>
+      <h1>Welcome to the Black Dragon Token Creator!</h1>
+    </div>
+  );
+}
